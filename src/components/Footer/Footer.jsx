37a8e493b0f12410537be87d5a4d4f9993fad2f3
bartek <bartosz.balinski@wp.pl>
2025-03-23 17:08:00 +0100

    Redesigned footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,33 @@
 import "./Footer.css";
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaEnvelope, FaPhone, FaArrowUp } from "react-icons/fa";
 
 export default function Footer() {
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     return (
         <footer className="footer">
             <div className="footer-container">
-                <p className="footer-text">© 2025 Brighties. Wszelkie prawa zastrzeżone.</p>
-                <div className="footer-socials">
+                {/* Sekcja kontaktowa po lewej */}
+                <div className="footer-info">
+                    <h3 className="footer-title">Kontakt</h3>
                     <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                        <FaFacebook />
+                        <FaFacebook className="icon" /> Facebook
                     </a>
-                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                        <FaInstagram />
-                    </a>
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                        <FaTwitter />
-                    </a>
+                    <p><FaEnvelope className="icon" /> brighties.pl@gmail.com
+                    </p>
+                    <p><FaPhone className="icon" /> +48 726 006 123
+                    </p>
                 </div>
+
+                {/* Sekcja copyright na środku */}
+                <p className="footer-text">© 2025 Brighties. Wszelkie prawa zastrzeżone.</p>
+
+                {/* Przycisk powrotu na górę po prawej */}
+                <button className="scroll-top" onClick={scrollToTop}>
+                    <FaArrowUp className="icon" /> Na górę
+                </button>
             </div>
         </footer>
     );
